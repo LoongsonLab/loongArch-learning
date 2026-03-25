b7f80fec7e3e72719fb5bbfd4d81a9960110f6cf
lyw19b <liyawei@loongson.cn>
2026-03-25 11:23:51 +0800

    refactor repository

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -39,6 +39,7 @@ export default defineConfig({
           { text: '编译原理', link: '/resources/courses/compiler/' },
         ],
       },
+      
       {
         text: '竞赛资源',
         collapsed: true,
@@ -47,16 +48,79 @@ export default defineConfig({
           { text: '编译系统设计赛', link: '/competitions/compiler-design/' },
         ],
       },
+      
       {
-        text: '文档与工具',
+        text: '龙架构相关文档',
         collapsed: true,
         items: [
-          { text: '参考手册', link: '/docs-la/' },
-          { text: 'LA工具链', link: '/toolchain/' },
-          { text: 'LA相关IP', link: '/practice/ip' },
-          { text: 'LA开源OS', link: '/practice/os' },
+          { text: '官方参考手册', link: '/docs-la/' },
+          { text: '其他参考手册', link: '/docs-la/others-ref' },
+          { text: '指令C语言实现', link: '/docs-la/others-la64-insn' },
+          { text: 'LoongArch相关IP', link: '/practice/ip' },
         ],
       },
+      
+      {
+        text: '龙架构工具链',
+        collapsed: true,
+        items: [
+          { text: '常用工具链', link: '/toolchain/' },
+          { text: 'libc库', link: '/toolchain/libc' },
+          { text: 'Linux内核', link: '/toolchain/linux-kernel' },
+          { text: 'U-boot', link: '/toolchain/uboot' },
+          { text: 'UEFI', link: '/toolchain/uefi' },
+        ],
+      },
+      
+      {
+        text: '龙架构开发平台',
+        collapsed: true,
+        items: [
+          { text: '2k1000LA星云版', link: '/platform/2k1000la' },
+          { text: '2k0300蜂鸟', link: '/platform/2k0300' },
+          { text: '2k3000开发板', link: '/platform/2k3000' },
+        ],
+      },
+      
+      {
+        text: '操作系统',
+        collapsed: true,
+        items: [
+          { text: '支持龙架构的发行版', link: '/os/release-os' },
+          { text: 'LoongArch开源Kernel', link: '/os/os-kernel' },
+          { text: '基于Rust的Kernel', link: '/os/os-kernel' },
+        ],
+      },
+      
+      {
+        text: 'LoongArch相关的仓库',
+        collapsed: true,
+        items: [
+          { text: '龙芯实验室', link: '/repos/loongsonlab' },
+          { text: '龙芯开源社区', link: '/repos/loongson-community' },
+        ],
+      },
+
+      {
+        text: '其他杂项',
+        collapsed: true,
+        items: [
+          { text: '支持龙架构的发行版', link: '/os/release-os' },
+          { text: 'LoongArch开源Kernel', link: '/os/os-kernel' },
+          { text: '基于Rust的Kernel', link: '/os/os-kernel' },
+        ],
+      },
+
+      {
+        text: '问题反馈',
+        collapsed: true,
+        items: [
+          { text: '如何提问', link: '/feedback/' },
+          { text: '实习机会', link: '/feedback/works' },
+          { text: '如何联系我们', link: '/feedback/contact' },
+        ],
+      },
+
     ],
     socialLinks: [],
   },
--- index.md
+++ index.md
@@ -7,11 +7,140 @@ outline: deep
 
 站内主要入口汇总
 
-**教学资源**：[计算机系统](/resources/courses/intro/) · [组成原理/体系结构](/resources/courses/arch/) · [操作系统](/resources/courses/os/) · [编译原理](/resources/courses/compiler/)
+## **教学资源**：
 
-**竞赛资源**：[操作系统设计赛](/competitions/os-design/) · [编译系统设计赛](/competitions/compiler-design/)
+- [计算机系统](/resources/courses/intro/)
 
-**文档与工具**：[参考手册](/docs-la/) · [LA 工具链](/toolchain/) · [LA相关IP](/practice/ip) · [LA开源OS](/practice/os)
+	主要包括一些课程的视频、课件及其实验代码等：
+	- 《计算机体系结构》—— 袁春风、鲍培明
+	- 《计算机系统实践教程》—— 袁春风、鲍培明
+
+
+- [组成原理/体系结构](/resources/courses/arch/)
+
+	主要包括一些课程的视频、课件及其实验代码等，另外包括一些教材，参考书籍等：
+	- 《计算机体系结构十讲》—— 胡伟武
+	- 《计算机体系结构六讲》—— 胡伟武
+	- 《龙架构体系结构实验课》—— 胡伟武
+
+	一些书籍与参考资料：
+	- 《计算机体系结构基础》—— 胡伟武、汪文祥等
+	- 《CPU设计实战：LoongArch版》—— 汪文祥、邢金璋
+
+
+- [操作系统](/resources/courses/os/)
+    
+    主要包括一些课程的视频、课件、教材及其实验代码等：
+    - 《深入理解操作系统》—— 宫晓利
+    - 《操作系统设计与实现（基于 LoongArch 架构）》—— 周庆国
+    - 《操作系统内核构建》—— 张羽、郭琼、张福新等
+
+
+- [编译原理](/resources/courses/compiler/)
+	
+	主要包括一些课程的视频、课件、教材及其实验代码等：
+	- 《编译系统设计与实现》—— 李诚
+
+## **竞赛资源**：
+
+- [操作系统设计赛](/competitions/os-design/)
+  
+  有关往年的操作系统相关内容：
+  - 往年的比赛的OS Kernel
+  - 往年比赛的优秀作品
+  - 往年比赛的报告
+  - 往年问题的FAQ
+
+- [编译系统设计赛](/competitions/compiler-design/)
+
+  有关往年的编译相关内容：
+  - 往年的比赛的优秀作品
+  - 往年比赛的报告
+  - 往年问题的FAQ
+
+
+## **龙架构相关文档**：
+
+- [官方参考手册](/docs-la/)
+	
+	主要涉及于龙架构相关的一些开发手册
+	- 龙芯架构参考手册 卷一：基础架构（64位/32位）
+	- 龙芯架构32位精简版参考手册（LoongArch32R）
+	- 应用程序二进制接口ABI-2.0
+	- 工具链规约手册
+	- 龙架构汇编语言编程手册-2024-10版本
+	
+  [其他参考手册](/docs-la/others-ref)
+	- 还要其他相关文档，具体可进入相关页查看
+
+  [LoongArch64指令C语言实现](/docs-la/others-la64-insn)
+	- 模拟器实现了LoongArch64的相关指令
+	- 如果官方手册的指令描述不清楚，可查看相关的C实现
+
+- [LoongArch相关IP](/practice/ip)
+	
+	主要涉及和龙架构相关的一些IP核：
+	- ChipLab：loongArch32R的一个实现SOC
+	- Open-500：
+	- OpenC910：
+
+
+## **LoongArch 工具链**
+
+- [LoongArch工具链](/toolchain/)
+	
+	主要是涉及龙架构的交叉编译器：
+	- GCC与其GLIBC
+	- GDB
+	- QEMU模拟器
+	- Clang编译器
+	- Rust
+	- Go
+	- .NET等
+
+- [LIBC库](/toolchain/libc)：
+  常用的C库：
+   - glibc
+   - musl
+   - newlib
+   - piclic
+
+- [Linux内核](/toolchain/linux-kernel)
+- [U-boot](/toolchain/uboot)
+- [UEFI](/toolchain/uefi)
+
+
+## **LoongArch开发版**
+
+涉及[常用的开发版相关资料](/platform)
+
+  - [2k1000LA星云版](/platform/2k1000la)
+
+  - [2k0300蜂鸟](/platform/2k0300)
+
+  - [2k3000开发板](/platform/2k3000)
+
+
+## **操作系统**
+
+[支持龙架构的发行版](/os/release-os)
+
+[LoongArch开源Kernel](/os/os-kernel)
+
+[基于Rust的Kernel](/os/os-kernel)
+
+## **LoongArch相关的仓库**
+
+[实验室仓库](/repos/loongsonlab)
+- https://github.com/orgs/LoongsonLab
+
+[龙芯开源社区](/repos/loongson-community)
+
+## **资讯动态**
+
+最近更新日期：2026-3-25
+
+
 
 ---
 
